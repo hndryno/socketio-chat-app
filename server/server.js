2f69bc73357e2fdb56cc261eeb7b0ac9d9d953f3
hendriyono <hendriyono97@gmail.com>
2020-04-29 19:32:24 +0800

    connect and disconnect event listener

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const http = require('http')
+const socketIO = require('socket.io')
 
 //membuat filenya static
 const publicPath = path.join(__dirname, '/../public')
@@ -8,9 +10,19 @@ const publicPath = path.join(__dirname, '/../public')
 app.use(express.static(publicPath))
 
 let port = process.env.PORT || 3000
+let server = http.createServer(app)
+let io = socketIO(server)
+
+io.on('connection', (socket) => {
+    console.log('new user connected')
+
+    socket.on('disconnect', (socket) => {
+        console.log('user was disconnect')
+    })
+})
 
 //listen port
-app.listen(`${port}`, () => {
+server.listen(`${port}`, () => {
     console.log(`server is up on port ${port}`)
 })
  
